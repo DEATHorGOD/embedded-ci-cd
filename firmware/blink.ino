9b851931428e451aa4337d47b8a8e873d05deb01
khoi-le-nguyen <lenguyenkhoi14112002@gmail.com>
2026-03-31 18:11:49 +0700

    Update blink.ino

--- firmware/blink.ino
+++ firmware/blink.ino
@@ -1,1 +1,11 @@
+void setup() {
+  // Thiết lập chân LED_BUILTIN là đầu ra
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // Bật LED
+  delay(1000);                       // Chờ 1 giây
+  digitalWrite(LED_BUILTIN, LOW);    // Tắt LED
+  delay(1000);                       // Chờ 1 giây
+}
